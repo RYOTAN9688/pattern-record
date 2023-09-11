2c2baf2b9c3b239e57d5fdf9bd69f2952f4f97c8
NAKAO RYOTA <premium.developer1122.gmail.com>
2023-09-11 16:50:46 +0900

    switch文にパターンを使用してレンダリングを制御する

--- lib/main.dart
+++ lib/main.dart
@@ -40,3 +40,30 @@ class DocumentScreen extends StatelessWidget {
     );
   }
 }
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+  const BlockWidget({required this.block, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? textStyle;
+    switch (block.type) {
+      case 'h1':
+        textStyle = Theme.of(context).textTheme.displayMedium;
+      //pかcheckboxに一致するかどうか（&&で良い)
+      case 'p' || 'checkbox':
+        textStyle = Theme.of(context).textTheme.bodyMedium;
+      //ワイルドカード（その他の全てに一致）
+      case '_':
+        textStyle = Theme.of(context).textTheme.bodySmall;
+    }
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: Text(
+        block.text,
+        style: textStyle,
+      ),
+    );
+  }
+}
